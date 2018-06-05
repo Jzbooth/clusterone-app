533a88a796c7308cd1148e4d1f2fc3eb872c53f6
Jared Booth <jzbooth@comcast.net>
2018-06-05 12:34:14 -0700

    saves

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from clusterone import get_data_path, get_logs_path
 
 #ClusterOne
 
-logs_path = get_logs_path('~/Documents/tf_logs/')
+#logs_path = get_logs_path('~/Documents/tf_logs/')
 
 # logging for gensim (set to INFO)
 #import logging
